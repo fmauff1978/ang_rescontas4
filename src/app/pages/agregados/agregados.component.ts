8e693b10200f349a73b1bf039d0337efc1e21f94
fabiomedeiros <fmauff1978@gmail.com>
2024-04-16 00:20:21 -0300

    correcao bug subagregados

--- src/app/pages/agregados/agregados.component.ts
+++ src/app/pages/agregados/agregados.component.ts
@@ -11,7 +11,7 @@ import { AtualizacaoService } from 'src/app/services/atualizacao.service';
 export class AgregadosComponent implements OnInit {
 
 
-  
+
  displayedColumns = ['demo-position','agregado','saldo', 'posicao']
  sub: any = {};
  sum: number;
@@ -28,7 +28,7 @@ export class AgregadosComponent implements OnInit {
  constructor(private fs: AngularFirestore, private as: AtualizacaoService, private sb: MatSnackBar){
 
   this.fs.collection('sub_agregados', (ref)=> ref.where('descricao', '==', 'bucket').orderBy('cod', 'desc')).valueChanges({idField: 'id'}).subscribe(value =>  {
-    this.sub = value; 
+    this.sub = value;
 
   //this.as.atualizarativo(this.sum);
  // this.openSnackBar("Ativo atualizado com sucesso!", "OK");
@@ -45,9 +45,9 @@ ngOnInit() {
 
       this.sumcompr = this.compr.reduce( function( a, b ) {
         return a + b.saldo;
-          }, 0 );   
-      
-  this.as.atualizarbucket("9W8ZVFbgQkkwCpAjEY8b", this.sumcompr);    
+          }, 0 );
+
+  this.as.atualizarbucket("9W8ZVFbgQkkwCpAjEY8b", this.sumcompr);
     })
 
     this.fs.collection('contas', (ref)=> ref.where("natureza", "==", "despesa").where("mod_despesa",'==', "gerenciável").where("ativa","==", true)).valueChanges({idField: 'id'}).subscribe(value =>  {
@@ -55,9 +55,9 @@ ngOnInit() {
 
       this.sumcompr = this.ger.reduce( function( a, b ) {
         return a + b.saldo;
-          }, 0 );   
-      
-  this.as.atualizarbucket("69pkU9qFFRR57xRLnBvi", this.sumcompr);    
+          }, 0 );
+
+  this.as.atualizarbucket("69pkU9qFFRR57xRLnBvi", this.sumcompr);
     })
 
     this.fs.collection('contas', (ref)=> ref.where("natureza", "==", "despesa").where("mod_despesa",'==', "off").where("ativa","==", true)).valueChanges({idField: 'id'}).subscribe(value =>  {
@@ -65,20 +65,20 @@ ngOnInit() {
 
       this.sumcompr = this.off.reduce( function( a, b ) {
         return a + b.saldo;
-          }, 0 );   
-      
-  this.as.atualizarbucket("OYGBZIs2mVu43LuFptr1", this.sumcompr);    
+          }, 0 );
+
+  this.as.atualizarbucket("OYGBZIs2mVu43LuFptr1", this.sumcompr);
     })
 
 
-    this.fs.collection('sub_agregados', (ref)=> ref.where("cod", "in", [38,39])).valueChanges({idField: 'id'}).subscribe(value =>  {
+    this.fs.collection('sub_agregados', (ref)=> ref.where("cod", "in", [37,38])).valueChanges({idField: 'id'}).subscribe(value =>  {
       this.sub = value;
 
       this.sum = this.sub.reduce( function( a, b ) {
         return a + b.saldo_atual;
-          }, 0 );   
-      
-  this.as.atualizarbucket("yDW6dJMmfT13jCBWUq2z", this.sum);    
+          }, 0 );
+
+  this.as.atualizarbucket("yDW6dJMmfT13jCBWUq2z", this.sum);
     })
 
     this.fs.collection('contas', (ref)=> ref.where("cod", "in", [32,40,37,35,39])).valueChanges({idField: 'id'}).subscribe(value =>  {
@@ -86,9 +86,9 @@ ngOnInit() {
 
       this.sum = this.sub.reduce( function( a, b ) {
         return a + b.saldo;
-          }, 0 );   
-      
-  this.as.atualizarbucket("4xArAMaYZVXgJzpLeX12", this.sum);    
+          }, 0 );
+
+  this.as.atualizarbucket("4xArAMaYZVXgJzpLeX12", this.sum);
     })
 
 
@@ -97,26 +97,26 @@ ngOnInit() {
 
       this.sum = this.sub.reduce( function( a, b ) {
         return a + b.saldo;
-          }, 0 );   
-      
-  this.as.atualizarbucket("ATWm8m90ckEWSgGVgih7", this.sum);    
+          }, 0 );
+
+  this.as.atualizarbucket("ATWm8m90ckEWSgGVgih7", this.sum);
     })
 
-    this.fs.collection('sub_agregados', (ref)=> ref.where("cod", "in", [41,39.5])).valueChanges({idField: 'id'}).subscribe(value =>  {
+    this.fs.collection('sub_agregados', (ref)=> ref.where("cod", "in", [41,39])).valueChanges({idField: 'id'}).subscribe(value =>  {
       this.sub = value;
 
       let receita = this.sub[0].saldo_atual;
       let despesa = this.sub[1].saldo_atual;
       this.superavit = receita - despesa;
-     
- 
+
+
     })
-     
-     
-        
-       
-  
-   
+
+
+
+
+
+
   }
 
 
